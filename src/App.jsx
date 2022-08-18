c779d27cdbe5e99cb02fd7ff81bc14bcbdae2a26
Jorge Munoz <jmunoz241995@gmail.com>
2022-08-17 18:42:56 -0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-    </div>
-  )
+    return (
+        <div className='App'>
+            <Navbar />
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <img src='/globe.png' className='navbar--globe' />
+            <p className='navbar--name'>my travel journal.</p>
+        </nav>
+    );
+}
+
+export default Navbar;
